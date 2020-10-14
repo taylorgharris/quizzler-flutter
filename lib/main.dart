1fc41868847843d1ba2fe129d5e369153790a41b
Taylor Harris <67606659+taylorgharris@users.noreply.github.com>
2020-10-14 16:59:24 -0700

    First edit
    Made an array of questions and answers and changes the text output of the question to be the first one in the array. Made an int called first that is set to 0.

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,15 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
+    List questions = [
+      ['You can lead a cow down stairs but not up stairs.', false],
+      ['Approximately one quarter of human bones are in the feet.', true],
+      ['A slug\'s blood is green.', true]
+    ];
+
+    int first = 0;
+
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -37,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[0][0],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
